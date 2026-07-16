7f97cb818ef735c3c7349adc003c833880439382
Raj Kumar <rk.singh7227@gmail.com>
2026-07-16 14:47:13 +0530

    Time: 1 ms (20.03%) | Memory: 9.3 MB (52.01%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+
+        for (char c : t) {
+            sum += c;
+        }
+
+        for (char c : s) {
+            sum -= c;
+        }
+
+        return char(sum);
+    }
+};
