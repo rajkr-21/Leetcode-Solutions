d61e400525a6f51588b2d5ec3f793f67e96d147b
Raj Kumar <rk.singh7227@gmail.com>
2026-08-01 22:46:04 +0530

    Time: 20 ms (69.79%) | Memory: 139 MB (91.51%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -8,18 +8,15 @@ public:
             if (!dq.empty() && dq.front() == i - k) {
                 dq.pop_front();
             }
-            
             while (!dq.empty() && nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
-            
             dq.push_back(i);
             
             if (i >= k - 1) {
                 result.push_back(nums[dq.front()]);
             }
         }
-        
         return result;
     }
 };
