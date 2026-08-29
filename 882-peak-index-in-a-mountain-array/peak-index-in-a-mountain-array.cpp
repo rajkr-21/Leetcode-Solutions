5f573ceb6a44342e84bca4f707f713e249bc88f7
Raj Kumar <rk.singh7227@gmail.com>
2026-08-29 12:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (79.11%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -15,18 +15,3 @@ public:
     }
 };
 
-// class Solution {
-//     public int peakIndexInMountainArray(int[] nums) {
-//         int l = 0, h = nums.length-1;
-//         while(l < h){
-//             int mid = l + (h - l) / 2;
-//             if(nums[mid] < nums[mid+1]){
-//                 l = mid + 1;
-//             }
-//             else{
-//                 h = mid;
-//             }
-//         }
-//         return l;
-//     }
-// }
