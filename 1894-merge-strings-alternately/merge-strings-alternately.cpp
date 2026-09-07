8c01836a32678117fff7c5b8e8c8d36fb142882f
Raj Kumar <rk.singh7227@gmail.com>
2026-09-07 23:45:00 +0530

    Time: 4 ms (32.02%) | Memory: 8.8 MB (9.75%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result = "";
-        int i = 0;
-        while (i < word1.length() || i < word2.length()) {
-            if (i < word1.length()) {
-                result += word1[i];
-            }
-            if (i < word2.length()) {
-                result += word2[i];
-            }
-            i++;
+        int n = word1.size(), m = word2.size();
+        string result;
+        result.reserve(n);
+
+        for (int i = 0; i < n || i < m; ++i) {
+            if (i < n) result += word1[i];
+            if (i < m) result += word2[i];
         }
+
         return result;
     }
 };
