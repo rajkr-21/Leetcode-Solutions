fd3665c6a247acb04e0d9abbec3231991f69e696
Raj Kumar <rk.singh7227@gmail.com>
2026-09-16 22:45:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.12%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,7 +3,8 @@ public:
     int hammingWeight(int n) {
         int count = 0;
         while(n){
-            n &= (n-1); count++;
+            n = n & (n-1); 
+            count++;
         }
         return count;
     }
