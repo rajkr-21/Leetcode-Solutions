caeffd9685e91b0ceaf25dfa7f16682dbd71042c
Raj Kumar <rk.singh7227@gmail.com>
2026-09-20 23:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (95.77%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int n= s.size();
         int ans = 0;
         for(int i=0; i<n; i++) 
-            ans += (i+1)*('z'-s[i]+1);
+            ans += (i+1) * ('z' - s[i] + 1);
         return ans;
     }
 };
